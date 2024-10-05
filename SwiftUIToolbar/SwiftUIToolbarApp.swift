83364b34fa2d8c31cd6a2dd0afc85ec333d69e30
manuaravind <manuaravindpta@gmail.com>
2024-10-05 12:50:22 +0530

    Initial Commit

---
+++ SwiftUIToolbar/SwiftUIToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToolbarApp.swift
+//  SwiftUIToolbar
+//
+//  Created by Manu Aravind on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
